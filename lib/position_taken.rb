c91234d221c32a049aa1bf2e98966beedc92204e
Vicki Kieu Nguyen <vickikieunguyen@gmail.com>
2020-11-02 21:32:05 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,10 +3,8 @@ def position_taken?(board,index)
   index.to_i
   if (board[index == " "]) || (board[index] == "") || (board[index] == nil)
     return false
-  elsif (board[index == "X"]) || (board[index == "O"])
-    return false
-
   else
+    expect (board[index == "X"] || board[index == "O"])
     return true
   end
 end
