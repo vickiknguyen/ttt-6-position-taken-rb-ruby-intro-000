71132022ae1412a095de369ab323ae4794a314d7
Vicki Kieu Nguyen <vickikieunguyen@gmail.com>
2020-11-02 21:39:51 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,10 @@
 # code your #position_taken? method here!
+def index
+  index = mo
+
 def position_taken?(board,index)
   index.to_i
-  if (board[index == " "] || board[index] == "" || board[index] == nil)
+  if (board[index.- 1] == " " || board[index.- 1] == "" || board[index.- 1] == nil)
     return false
   else
     expect (board[index == "X"] || board[index == "O"])
